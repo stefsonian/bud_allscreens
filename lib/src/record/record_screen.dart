76b89f0e1f3cb0b22f333c0fc2a3b96e345d5120
Stefan Stefansen <mail@stefanstefansen.com>
2019-08-06 11:20:25 +1000

    Before adding provider

--- lib/src/record/record_screen.dart
+++ lib/src/record/record_screen.dart
@@ -13,20 +13,33 @@ class RecordScreen extends StatefulWidget {
 }
 
 class _RecordScreenState extends State<RecordScreen> {
+  double screenHeight;
   int activeWidget = 0;
   List<Widget> widgets;
+  double bufferHeight;
 
   didChangeDependencies() {
     super.didChangeDependencies();
-    widgets = [_amountAndNumpad(), _amountAndMainCategory()];
+    screenHeight = MediaQuery.of(context).size.height;
+    setBufferHeight();
+    widgets = [Numpad(), _mainCategoryOnly(), _subcategoryOnly()];
+  }
+
+  setBufferHeight() {
+    double newHeight = 16.0;
+    if (activeWidget == 0) {
+      newHeight = screenHeight < 830 ? 28.0 : 58.0;
+    }
+    setState(() => bufferHeight = newHeight);
   }
 
   switchWidget() {
     if (activeWidget == widgets.length - 1) {
       setState(() => activeWidget = 0);
-      return;
+    } else {
+      setState(() => activeWidget++);
     }
-    setState(() => activeWidget++);
+    setBufferHeight();
     print('Switching to widget $activeWidget');
   }
 
@@ -40,19 +53,26 @@ class _RecordScreenState extends State<RecordScreen> {
               child: Text('Test'),
               onPressed: () => switchWidget(),
             ),
-            SizedBox(
-              height: 70,
-              child: GradientBoxSimple(
-                child: RecordOptions(),
-              ),
+            _recordOptions(),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: bufferHeight,
             ),
+            _amount(),
             Expanded(
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 60),
-                // transitionBuilder: (Widget child, Animation<double> animation) {
-                //   return ScaleTransition(child: child, scale: animation);
-                // },
-                child: widgets[activeWidget],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  AnimatedSwitcher(
+                    duration: Duration(milliseconds: 300),
+                    transitionBuilder:
+                        (Widget child, Animation<double> animation) {
+                      return ScaleTransition(child: child, scale: animation);
+                    },
+                    child: widgets[activeWidget],
+                  ),
+                  if (activeWidget == 0) _nextButton(),
+                ],
               ),
             ),
           ],
@@ -81,104 +101,184 @@ class _RecordScreenState extends State<RecordScreen> {
     );
   }
 
-  Widget _amountAndNumpad() {
-    return Column(
-      key: Key('0'),
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: <Widget>[
-        SizedBox(
-          height: 70,
-          child: DisplayAmount(),
-        ),
-        Numpad(),
-        SizedBox(
-          width: 150,
-          child: FloatingActionButton.extended(
-            label: Text(
-              'Next',
-              textScaleFactor: 1.5,
-            ),
-            backgroundColor: Colors.yellow,
-            foregroundColor: col_purple,
-            onPressed: () {},
-          ),
-        ),
-      ],
+  Widget _amount() {
+    return SizedBox(
+      height: 70,
+      child: DisplayAmount(),
     );
   }
 
-  Widget _amountAndMainCategory() {
-    return Container(
-      margin: EdgeInsets.only(top: 12),
-      child: Column(
-        key: Key('1'),
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 70,
-            child: DisplayAmount(),
-          ),
-          Expanded(
-            child: Center(
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Choose a category',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        wordSpacing: 2,
-                        letterSpacing: 2,
-                      ),
-                    ),
-                    SizedBox(height: 60),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        IkonButton(
-                          icon: Icons.restaurant,
-                          color: col_purple,
-                          backColor: Colors.white,
-                        ),
-                        IkonButton(
-                          icon: Icons.hotel,
-                          color: col_purple,
-                          backColor: Colors.white,
-                        ),
-                        IkonButton(
-                          icon: Icons.train,
-                          color: col_purple,
-                          backColor: Colors.white,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 50),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        IkonButton(
-                          icon: Icons.camera_alt,
-                          color: col_purple,
-                          backColor: Colors.white,
-                        ),
-                        IkonButton(
-                          icon: Icons.scatter_plot,
-                          color: col_purple,
-                          backColor: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
+  Widget _buffer(double screenHeight) {
+    double boxHeight = 28.0;
+    if (screenHeight > 830) boxHeight = 84.0;
+    return SizedBox(height: boxHeight);
+  }
+
+  Widget _recordOptions() {
+    return SizedBox(
+      height: 70,
+      child: GradientBoxSimple(
+        child: RecordOptions(),
       ),
     );
   }
 
-  Widget _SubcategoryOnly() {}
+  Widget _nextButton() {
+    return SizedBox(
+      width: 150,
+      child: FloatingActionButton.extended(
+        label: Text(
+          'Next',
+          textScaleFactor: 1.5,
+        ),
+        backgroundColor: Colors.yellow,
+        foregroundColor: col_purple,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _mainCategoryOnly() {
+    return Center(
+      key: Key('mainCat'),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Choose a category',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                wordSpacing: 2,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IkonButton(
+                  icon: Icons.restaurant,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.hotel,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.train,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IkonButton(
+                  icon: Icons.camera_alt,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.scatter_plot,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _subcategoryOnly() {
+    return Center(
+      key: Key('subCats'),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Choose a sub-category',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                wordSpacing: 2,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IkonButton(
+                  icon: Icons.train,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.tram,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.directions_bus,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IkonButton(
+                  icon: Icons.directions_subway,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.flight,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.directions_boat,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IkonButton(
+                  icon: Icons.directions_car,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.directions_bike,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+                IkonButton(
+                  icon: Icons.directions_run,
+                  color: col_purple,
+                  backColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
