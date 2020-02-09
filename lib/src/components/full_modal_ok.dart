703388e8fc6a129469bda8aa69ccf286aa3b519b
stefsonian <mail@stefanstefansen.com>
2020-02-10 10:51:20 +1100

    Before change to full screen modal dialogs

---
+++ lib/src/components/full_modal_ok.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class FullModalOk extends ModalRoute<void> {
+  FullModalOk({
+    @required this.header,
+    @required this.body,
+    @required this.onOkTap,
+    @required this.buttonColor,
+  });
+  final Widget header;
+  final Widget body;
+  final Function onOkTap;
+  final Color buttonColor;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 500);
+
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissible => false;
+
+  @override
+  Color get barrierColor => Colors.black.withOpacity(0.8);
+
+  @override
+  String get barrierLabel => null;
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Widget buildPage(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+  ) {
+    // This makes sure that text and other content follows the material style
+    return Material(
+      type: MaterialType.transparency,
+      // make sure that the overlay content is not cut off
+      child: SafeArea(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              header,
+              Expanded(child: body),
+              _buttons(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttons(BuildContext context) {
+    return Container(
+      height: 70,
+      // padding: EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            width: 1,
+            color: buttonColor.withOpacity(0.4),
+          ),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () => Navigator.pop(context),
+              child: Icon(Icons.close, color: buttonColor, size: 34),
+              // child: Text('Cancel',
+              //     style: _textStyle.copyWith(color: buttonColor)),
+            ),
+          ),
+          // Container(
+          //   width: 1,
+          //   color: buttonColor.withOpacity(0.4),
+          // ),
+          Expanded(
+            child: FlatButton(
+              onPressed: onOkTap,
+              child: Icon(Icons.check, color: buttonColor, size: 34),
+              // child: Text('OK', style: _textStyle.copyWith(color: buttonColor)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 18,
+    letterSpacing: 1.8,
+  );
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    // You can add your own animations for the overlay content
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+
+  // @override
+  // Widget buildTransitions(BuildContext context, Animation<double> animation,
+  //     Animation<double> secondaryAnimation, Widget child) {
+  //   // You can add your own animations for the overlay content
+  //   return FadeTransition(
+  //     opacity: animation,
+  //     child: ScaleTransition(
+  //       scale: animation,
+  //       child: child,
+  //     ),
+  //   );
+  // }
+}
